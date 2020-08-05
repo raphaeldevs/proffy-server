429e38d717e75025c3c1b339ec9d5d89767ecdac
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2020-08-05 14:41:32 -0300

    Dia 2

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+console.clear();
+
+console.log("Hello, I am Server 🎈");
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(routes);
+
+app.listen(3333);
